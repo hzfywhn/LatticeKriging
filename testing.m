9345bb1fc0560fa4f4c2a63686e06e59887d6c10
hzfywhn <hzfywhn@g.clemson.edu>
2021-02-03 17:11:28 -0500

    changed statistical mean

--- testing.m
+++ testing.m
@@ -3,19 +3,7 @@ x2 = 4;
 y1 = -2;
 y2 = 2;
 
-delta = 0.1;
-x0 = x1: delta: x2;
-y0 = y1: delta: y2;
-[y, x] = meshgrid(y0, x0);
-z = 1 ./ (1 + (x-1).^2 + y.^2) - 1 ./ (1 + (x+1).^2 + y.^2);
-vx = -2*(x-1) ./ (1 + (x-1).^2 + y.^2).^2 + 2*(x+1) ./ (1 + (x+1).^2 + y.^2).^2;
-vy = -2*y ./ (1 + (x-1).^2 + y.^2).^2 + 2*y ./ (1 + (x+1).^2 + y.^2).^2;
-v = sqrt(vx.^2 + vy.^2);
-cosx = vx ./ v;
-cosy = vy ./ v;
-obs = struct('loc', [x(:) y(:)], 'azim', [cosx(:) cosy(:)], 'val', v(:), 'err', ones(numel(v), 1));
-
-delta = 0.2;
+delta = 0.5;
 x0 = x1: delta: x2;
 y0 = y1: delta: y2;
 nx = length(x0);
@@ -45,8 +33,35 @@ basis = {struct('loc', loc, 'connect', con, 'centerweight', 4.01, 'delta', delta
 
 normalization = false;
 rho = 1;
-derivative = true;
-[y, W, Z, Q, phi] = constants(obs, basis, normalization, rho, derivative);
+
+delta = 0.1;
+x0 = x1: delta: x2;
+y0 = y1: delta: y2;
+[y, x] = meshgrid(y0, x0);
+n = numel(x);
+sub = randsample(n, floor(n/10));
+x = x(sub);
+y = y(sub);
+z = 1 ./ (1 + (x-1).^2 + y.^2) - 1 ./ (1 + (x+1).^2 + y.^2);
+vx = -2*(x-1) ./ (1 + (x-1).^2 + y.^2).^2 + 2*(x+1) ./ (1 + (x+1).^2 + y.^2).^2;
+vy = -2*y ./ (1 + (x-1).^2 + y.^2).^2 + 2*y ./ (1 + (x+1).^2 + y.^2).^2;
+cosx = ones(size(vx));
+cosy = zeros(size(vy));
+v = vx.*cosx + vy.*cosy;
+obs = struct('loc', [x y], 'azim', [cosx cosy], 'val', v, 'err', ones(numel(v), 1));
+
+subplot(211)
+scatter(x, y, [], z, 'filled')
+hold on
+quiver(x, y, vx.*cosx, vy.*cosy)
+hold off
+colorbar
+
+y = obs.val;
+ind = 1: size(obs.val, 1);
+W = sparse(ind, ind, obs.err);
+Z = ones(size(v));
+[Q, phi] = combineMR(obs, basis, normalization, rho, true);
 lambda = optimize(y, W, Z, Q, phi, exp(-9), exp(5), 5e-3);
 [d, c, rhoMLE, likelihood, M] = kriging(lambda, y, W, Z, Q, phi);
 
@@ -57,26 +72,13 @@ y0 = y1: delta: y2;
 z = 1 ./ (1 + (x-1).^2 + y.^2) - 1 ./ (1 + (x+1).^2 + y.^2);
 vx = -2*(x-1) ./ (1 + (x-1).^2 + y.^2).^2 + 2*(x+1) ./ (1 + (x+1).^2 + y.^2).^2;
 vy = -2*y ./ (1 + (x-1).^2 + y.^2).^2 + 2*y ./ (1 + (x+1).^2 + y.^2).^2;
-[m, sd] = prediction([x(:) y(:)], basis, normalization, rho, lambda, Z, Q, phi, M, d, c, rhoMLE);
+[~, phi1] = combineMR(struct('loc', [x(:) y(:)]), basis, normalization, rho, false);
+[m, sd] = prediction(ones(numel(z), 1), phi1, lambda, Z, Q, phi, M, d, c, rhoMLE);
 
-subplot('Position', [0.05 0.05 0.4 0.9])
-h = pcolor(x, y, z);
-h.EdgeColor = 'none';
-hold on
-quiver(x, y, -vx, -vy, 'k', 'LineWidth', 1)
-hold off
-title('Electric Field Input')
-axis([-4 4 -2 2])
-caxis([-0.8 0.8])
-colormap('jet')
-subplot('Position', [0.5 0.05 0.4 0.9])
+subplot(212)
 h = pcolor(x, y, reshape(m, [length(x0) length(y0)]));
 h.EdgeColor = 'none';
-title('Electric Potential Output')
-axis([-4 4 -2 2])
-caxis([-0.8 0.8])
-colormap('jet')
-colorbar('Position', [0.92 0.05 0.03 0.9])
+colorbar
 
 function lambda = optimize(y, W, Z, Q, phi, xmin, xmax, tol)
     function likelihood = LK(l)
